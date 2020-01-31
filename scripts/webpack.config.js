5ad4236eb2dc87ead260f17330a232c8333d407a
rookiesam <zhd1109@qq.com>
2020-01-31 14:12:30 +0800

    js css webpack处理

---
+++ scripts/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(process.cwd(), 'dist'),
+        filename: 'static/js/[name].[chunkHash:8].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Tutorials',
+            template: 'public/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'static/css/[name].[chunkHash:8].css'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+        ]
+    }
+}
