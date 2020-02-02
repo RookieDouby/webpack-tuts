dade5a910d48124db6f3d8153479ee0e9fd79a9b
rookiesam <zhd1109@qq.com>
2020-02-02 17:04:34 +0800

    添加babel处理es6代码

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -65,6 +65,15 @@ module.exports = {
                         }
                     }
                 ]
+            },{
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     },
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './index.less'
 import './app.css'
 
+import { add } from './add'
+
+add(1,2)
+
 console.log('hello world !!!! Welcome to  webpack')
