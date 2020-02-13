5fd0bb11d2ce02a00766dcd6d99886bcb51abc2b
张红豆 <zhanghongdou@enn.cn>
2020-02-13 23:34:32 +0800

    分离出dev&prod

---
+++ scripts/webpack.config.dev.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(process.cwd(), 'dist'),
+        filename: 'js/[name].[chunkHash:8].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack Tutorials',
+            template: 'public/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[chunkHash:8].css'
+        }),
+        new CopyWebpackPlugin([
+            { from: path.resolve(process.cwd(), 'src/static'), to: path.resolve(process.cwd(), 'dist/static') }
+        ]) 
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.less$/,
+                use:[
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'less-loader',
+                        options: {
+
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    // {
+                    //     loader: 'file-loader',
+                    //     options: {
+                    //         name: 'images/[name].[ext]',
+                    //         publicPath: '/'
+                    //     },
+                    // },
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 512,
+                            name: 'images/[name].[ext]',
+                            publicPath: '/'
+                        }
+                    }
+                ]
+            },{
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+    }
+}
